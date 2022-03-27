1c755946555ba0359df778c2bce6326a8eb378e7
Siwon  Kim <siwon@usf.edu>
2022-03-27 11:57:57 -0400

    Updates

---
+++ precache-manifest.46a6c76b7ec6423d07680fd75e50a5ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d0b58884e4c57450cfa",
+    "url": "/NFT-Bullery/static/js/main.2d0b5888.chunk.js"
+  },
+  {
+    "revision": "d4dfddad9693ce82f731",
+    "url": "/NFT-Bullery/static/js/1.d4dfddad.chunk.js"
+  },
+  {
+    "revision": "c3ca240a6cc60481d9a5",
+    "url": "/NFT-Bullery/static/js/2.c3ca240a.chunk.js"
+  },
+  {
+    "revision": "587afc132644dc3f3581",
+    "url": "/NFT-Bullery/static/js/runtime~main.587afc13.js"
+  },
+  {
+    "revision": "0919a34490dd5bd69879ebe2198d6adf",
+    "url": "/NFT-Bullery/index.html"
+  }
+];
